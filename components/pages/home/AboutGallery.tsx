bb91d7da19cf9906a848c326e15869a1a2f2353d
pavel11988 <kossoua88@gmail.com>
2022-11-27 14:56:25 +0200

    init

--- components/pages/home/AboutGallery.tsx
+++ components/pages/home/AboutGallery.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import { motion } from "framer-motion";
+import { v4 as uuidv4 } from "uuid";
 
 interface ImageProps {
   company: string;
@@ -13,7 +14,15 @@ interface AboutGalleryProps {
   images: ImageProps[];
 }
 
-const separateArray = (images: ImageProps[]) => {
+interface changedImageProps {
+  // id: 'string'
+  company: string;
+  src: string;
+  width: number;
+  height: number;
+}
+
+const separateArray = (images: changedImageProps[]) => {
   if (images.length > 8) images = images.slice(0, 8);
   else if (images.length < 8) {
     let newImages = [...images];
@@ -21,34 +30,61 @@ const separateArray = (images: ImageProps[]) => {
     if (newImages.length > 8) images = newImages.slice(0, 8);
     else images = newImages;
   }
-  return [images.slice(0, 4), images.slice(4, 8)];
+  const changedImages = images.map((image) => {
+    const id = uuidv4();
+    return {
+      ...image,
+      id,
+    };
+  });
+  return [changedImages.slice(0, 4), changedImages.slice(4, 8)];
 };
 
 const AboutGallery: FC<AboutGalleryProps> = ({ images }) => {
-  const [leftArray, rightArray] = separateArray(images);
+  const [hovered, setHovered] = useState<null | string>(null);
+
+  const [leftArray, rightArray] = useMemo(
+    () => separateArray(images),
+    [images]
+  );
+
+  // useEffect(() => {
+  //   console.log(hovered);
+  // }, [hovered]);
+
+  // useEffect(() => {
+  //   console.log("rerender");
+  // }, [images]);
 
   return (
     <motion.div className="flex images-container">
       <motion.ul className="images-list">
         {leftArray.map((image, i) => {
-          const { company, src, width, height } = image;
+          const { id, company, src, width, height } = image;
           return (
             <motion.li
+              onMouseEnter={() => setHovered(id)}
+              onMouseLeave={() => setHovered(null)}
               key={i}
               animate={{ y: 50 }}
               transition={{ delay: 0 }}
-              whileHover={{
-                position: "relative",
-                zIndex: 11,
-                y: i > 1 ? -100 : 120,
-                x: 30,
-                scale: [1, 1.5],
-                transition: {
-                  duration: 0.2,
-                },
-              }}
+              className={"relative"}
             >
-              <Image
+              <motion.img
+                animate={
+                  id === hovered
+                    ? {
+                        position: "relative",
+                        zIndex: 25,
+                        y: i > 1 ? -100 : 120,
+                        x: 30,
+                        scale: [1, 1.5],
+                        transition: {
+                          duration: 0.2,
+                        },
+                      }
+                    : { position: "relative", zIndex: 10 }
+                }
                 className={"m-5"}
                 src={src}
                 alt={company}
@@ -61,24 +97,31 @@ const AboutGallery: FC<AboutGalleryProps> = ({ images }) => {
       </motion.ul>
       <motion.ul>
         {rightArray.map((image, i) => {
-          const { company, src, width, height } = image;
+          const { id, company, src, width, height } = image;
           return (
             <motion.li
+              onMouseEnter={() => setHovered(id)}
+              onMouseLeave={() => setHovered(null)}
               key={i}
               animate={{ y: -50 }}
               transition={{ delay: 0 }}
-              whileHover={{
-                position: "relative",
-                zIndex: 11,
-                y: i > 1 ? -100 : 120,
-                x: -30,
-                scale: [1, 1.5],
-                transition: {
-                  duration: 0.2,
-                },
-              }}
             >
-              <Image
+              <motion.img
+                animate={
+                  id === hovered
+                    ? {
+                        position: "relative",
+
+                        y: i > 1 ? -100 : 120,
+                        x: -30,
+                        zIndex: 25,
+                        scale: [1, 1.5],
+                        transition: {
+                          duration: 0.2,
+                        },
+                      }
+                    : {}
+                }
                 className={"m-5"}
                 src={src}
                 alt={company}
